3bbc10f7ca15ceace0b02a2f20c968de0759ee15
Jules Scholler <jules.scholler@gmail.com>
2018-03-09 09:30:10 +0100

    added colormap build function

--- im.py
+++ im.py
@@ -14,6 +14,8 @@ from sklearn import preprocessing
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw 
+import matplotlib.pyplot as plt
+
 
 def normalize(data):
     """ The camera sensor drift and we correct it by normalizing each image by the total energy. """
@@ -251,3 +253,26 @@ def write_text(imPath, text, position=(0,0), color=(255,255,255), size=100):
     font = ImageFont.truetype("arial.ttf",100)
     draw.text(position, text, color, font=font)
     a.save(imPath[0:-4] + '_text.tif')
+    
+def build_colormap(Lx=50, Ly=1000, fs=2):
+    color = np.linspace(1,0,Ly)
+    H = np.matlib.repmat(color,Lx,1).transpose()
+    
+    saturation = np.linspace(1, 0, Lx)
+    S = np.matlib.repmat(saturation, Ly, 1)
+    V = np.ones((Ly,Lx))*0.7
+    
+    colormap = {}
+    colormap['image'] = hsv2rgb(np.dstack((H,S,V)))
+    colormap['freq'] = np.linspace(0,fs/2,Ly)
+    colormap['df'] = np.linspace(0,fs/2,Lx)
+    
+    dx = (colormap['df'][1]-colormap['df'][0])/2.
+    dy = (colormap['freq'][1]-colormap['freq'][0])/2.
+    extent = [colormap['df'][0]-dx, colormap['df'][-1]+dx, colormap['freq'][0]-dy, colormap['freq'][-1]+dy]
+    
+    plt.imshow(colormap['image'], extent=extent)
+    plt.xlabel('Frequency bandwidth [Hz]')
+    plt.ylabel('Median frequency [Hz]')
+    
+    return colormap
